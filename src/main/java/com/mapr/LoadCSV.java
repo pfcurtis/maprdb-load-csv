1d7f7c3f35de7b93b29d5bfb8b9d1517c8abfe7a
Paul Curtis <pcurtis@mapr.com>
2015-06-29 15:49:39 -0400

    Added a timer to slow the put() to the table. I may want to add a commit or aset auto-flush.

--- src/main/java/com/mapr/LoadCSV.java
+++ src/main/java/com/mapr/LoadCSV.java
@@ -22,7 +22,7 @@ import org.apache.commons.logging.LogFactory;
 
 public class LoadCSV {
 
-    private static String TABLE_NAME = "/user/mapr/test";
+    private static String TABLE_NAME = "/user/mapr/flightdata";
     private static String TABLE_CF = "cf1";
     private static String CSV_FILE = "364949592_T_ONTIME.csv";
     private static String[] columns = {"YEAR","MONTH","FL_DATE","CARRIER","FL_NUM","ORIGIN","ORIGIN_CITY_NAME",
@@ -36,6 +36,7 @@ public class LoadCSV {
         FileReader in = null;
         HTable table = null;
         Iterable<CSVRecord>parser  = null;
+        int count = 0;
 
         try {
             Configuration config = HBaseConfiguration.create();
@@ -59,17 +60,17 @@ public class LoadCSV {
             String flight_date = record.get("FL_DATE");
 
 // Create a key value that's unique
-            log.debug("Record: " + carrier + "-" + flight_num + "-" + flight_date);
             Put put = new Put(Bytes.toBytes(carrier + "-" + flight_num + "-" + flight_date));
+            log.debug("Key: " + carrier + "-" + flight_num + "-" + flight_date);
 
             for (String columnName : columns) {
                 log.debug("Add columns: "+TABLE_CF+":"+columnName+" = "+record.get(columnName));
                 put.add(Bytes.toBytes(TABLE_CF), Bytes.toBytes(columnName), Bytes.toBytes(record.get(columnName)));
             }
             try {
-                log.debug("PUT");
                 table.put(put);
-                log.debug("PUT complete");
+                log.info("Record: " + carrier + "-" + flight_num + "-" + flight_date);
+                Thread.sleep(1000);
             } catch (Exception e3) {
                 e3.printStackTrace(System.out);
                 System.exit(1);
