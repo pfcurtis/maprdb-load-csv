27e5acddb080670f0a254508ae4546d9f09ac336
Paul Curtis <pcurtis@mapr.com>
2015-07-09 11:43:13 -0400

    Updated to perform the correct data type puts

--- src/main/java/com/mapr/LoadCSV.java
+++ src/main/java/com/mapr/LoadCSV.java
@@ -21,15 +21,21 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 public class LoadCSV {
+    /*
+    "YEAR","MONTH","DAY_OF_MONTH","DAY_OF_WEEK","UNIQUE_CARRIER","ORIGIN","ORIGIN_CITY_NAME","ORIGIN_STATE_NM","DEST","DEST_CITY_NAME","DEST_STATE_NM","CRS_DEP_TIME","DEP_TIME","DEP_DELAY","CRS_ARR_TIME","ARR_TIME","ARR_DELAY","ACTUAL_ELAPSED_TIME","AIR_TIME"
+    */
 
-    private static String TABLE_NAME = "/user/mapr/flightdata";
+
+
+    private static String TABLE_NAME = "/user/mapr/flight_data";
     private static String TABLE_CF = "cf1";
-    private static String CSV_FILE = "364949592_T_ONTIME.csv";
-    private static String[] columns = {"YEAR","MONTH","FL_DATE","CARRIER","FL_NUM","ORIGIN","ORIGIN_CITY_NAME",
-                                       "ORIGIN_STATE_ABR","DEST","DEST_CITY_NAME","DEST_STATE_ABR","DEP_TIME",
-                                       "DEP_DELAY_NEW","ARR_TIME","ARR_DELAY_NEW","CANCELLED",
-                                       "DIVERTED"
-                                      };
+    private static String CSV_FILE = "flights.csv";
+    private static String[] columns = {"YEAR","MONTH","DAY_OF_MONTH","DAY_OF_WEEK","UNIQUE_CARRIER","ORIGIN","ORIGIN_CITY_NAME","ORIGIN_STATE_NM","DEST","DEST_CITY_NAME","DEST_STATE_NM","CRS_DEP_TIME","DEP_TIME","DEP_DELAY","CRS_ARR_TIME","ARR_TIME","ARR_DELAY","ACTUAL_ELAPSED_TIME","AIR_TIME"};
+    /*  private static String[] columns = {"YEAR","MONTH","FL_DATE","CARRIER","FL_NUM","ORIGIN","ORIGIN_CITY_NAME",
+                                           "ORIGIN_STATE_ABR","DEST","DEST_CITY_NAME","DEST_STATE_ABR","DEP_TIME",
+                                           "DEP_DELAY_NEW","ARR_TIME","ARR_DELAY_NEW","CANCELLED",
+                                           "DIVERTED"
+                                          }; */
 
     public static void main(String[] args) {
         Log log = LogFactory.getLog(LoadCSV.class);
@@ -55,22 +61,60 @@ public class LoadCSV {
             System.exit(1);
         }
         for (CSVRecord record : parser) {
-            String carrier = record.get("CARRIER");
-            String flight_num = record.get("FL_NUM");
-            String flight_date = record.get("FL_DATE");
 
 // Create a key value that's unique
-            Put put = new Put(Bytes.toBytes(carrier + "-" + flight_num + "-" + flight_date));
-            log.debug("Key: " + carrier + "-" + flight_num + "-" + flight_date);
+            Put put = new Put(Bytes.toBytes(String.valueOf(count++)));
 
-            for (String columnName : columns) {
-                log.debug("Add columns: "+TABLE_CF+":"+columnName+" = "+record.get(columnName));
-                put.add(Bytes.toBytes(TABLE_CF), Bytes.toBytes(columnName), Bytes.toBytes(record.get(columnName)));
-            }
+            put.add(Bytes.toBytes(TABLE_CF), Bytes.toBytes("ARR_TIME"), Bytes.toBytes(record.get("ARR_TIME")));
+            put.add(Bytes.toBytes(TABLE_CF), Bytes.toBytes("CRS_ARR_TIME"), Bytes.toBytes(record.get("CRS_ARR_TIME")));
+            put.add(Bytes.toBytes(TABLE_CF), Bytes.toBytes("CRS_DEP_TIME"), Bytes.toBytes(record.get("CRS_DEP_TIME")));
+            put.add(Bytes.toBytes(TABLE_CF), Bytes.toBytes("DEP_TIME"), Bytes.toBytes(record.get("DEP_TIME")));
+            put.add(Bytes.toBytes(TABLE_CF), Bytes.toBytes("DEST"), Bytes.toBytes(record.get("DEST")));
+            put.add(Bytes.toBytes(TABLE_CF), Bytes.toBytes("DEST_CITY_NAME"), Bytes.toBytes(record.get("DEST_CITY_NAME")));
+            put.add(Bytes.toBytes(TABLE_CF), Bytes.toBytes("DEST_STATE_NM"), Bytes.toBytes(record.get("DEST_STATE_NM")));
+            put.add(Bytes.toBytes(TABLE_CF), Bytes.toBytes("ORIGIN"), Bytes.toBytes(record.get("ORIGIN")));
+            put.add(Bytes.toBytes(TABLE_CF), Bytes.toBytes("ORIGIN_CITY_NAME"), Bytes.toBytes(record.get("ORIGIN_CITY_NAME")));
+            put.add(Bytes.toBytes(TABLE_CF), Bytes.toBytes("ORIGIN_STATE_NM"), Bytes.toBytes(record.get("ORIGIN_STATE_NM")));
+            put.add(Bytes.toBytes(TABLE_CF), Bytes.toBytes("UNIQUE_CARRIER"), Bytes.toBytes(record.get("UNIQUE_CARRIER")));
+
+            try {
+                put.add(Bytes.toBytes(TABLE_CF), Bytes.toBytes("ACTUAL_ELAPSED_TIME"),
+                        Bytes.toBytes(Integer.getInteger(record.get("ACTUAL_ELAPSED_TIME")).longValue()));
+            } catch (Exception e11) {}
+
+            try {
+                put.add(Bytes.toBytes(TABLE_CF), Bytes.toBytes("AIR_TIME"),
+                        Bytes.toBytes(Integer.getInteger(record.get("AIR_TIME")).longValue()));
+            } catch (Exception e12) {}
+            try {
+                put.add(Bytes.toBytes(TABLE_CF), Bytes.toBytes("ARR_DELAY"),
+                        Bytes.toBytes(Integer.getInteger(record.get("ARR_DELAY")).longValue()));
+            } catch (Exception e13) {}
+            try {
+                put.add(Bytes.toBytes(TABLE_CF), Bytes.toBytes("DAY_OF_MONTH"),
+                        Bytes.toBytes(Integer.getInteger(record.get("DAY_OF_MONTH")).longValue()));
+            } catch (Exception e14) {}
+            try {
+                put.add(Bytes.toBytes(TABLE_CF), Bytes.toBytes("DAY_OF_WEEK"),
+                        Bytes.toBytes(Integer.getInteger(record.get("DAY_OF_WEEK")).longValue()));
+            } catch (Exception e15) {}
+            try {
+                put.add(Bytes.toBytes(TABLE_CF), Bytes.toBytes("DEP_DELAY"),
+                        Bytes.toBytes(Integer.getInteger(record.get("DEP_DELAY")).longValue()));
+            } catch (Exception e16) {}
+            try {
+                put.add(Bytes.toBytes(TABLE_CF), Bytes.toBytes("MONTH"),
+                        Bytes.toBytes(Integer.getInteger(record.get("MONTH")).longValue()));
+            } catch (Exception e17) {}
+            try {
+                put.add(Bytes.toBytes(TABLE_CF), Bytes.toBytes("YEAR"),
+                        Bytes.toBytes(Integer.getInteger(record.get("YEAR")).longValue()));
+            } catch (Exception e18) {}
+
             try {
                 table.put(put);
-                log.info("Record: " + carrier + "-" + flight_num + "-" + flight_date);
-                Thread.sleep(1000);
+                log.info("Record: " + count);
+                Thread.sleep(100);
             } catch (Exception e3) {
                 e3.printStackTrace(System.out);
                 System.exit(1);
